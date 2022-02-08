51eebc2c70f18c79c51922d2f1f4a6ae83ca56c2
thanhchuong99 <nguyenthanhchuonghth@gmail.com>
2022-02-08 19:53:34 +0700

    init

---
+++ src/redux/selectors.js
@@ -0,0 +1,39 @@
+import { createSelector } from "reselect";
+
+export const todoListSelector = (state) => state.todoList;
+export const searchTextSelector = (state) => state.filters.search;
+export const filterStatusSelector = (state) => state.filters.status;
+export const filterPrioritySelector = (state) => state.filters.priority;
+
+export const todoRemainingSelector = createSelector(
+  todoListSelector,
+  searchTextSelector,
+  filterStatusSelector,
+  filterPrioritySelector,
+  (todoList, searchText, status, priorities) => {
+    const allTodoList = todoList.filter((todo) => {
+      return todo.name.includes(searchText);
+    });
+    let todoRemaining;
+    switch (status) {
+      case "Todo":
+        todoRemaining = allTodoList.filter((todo) => !todo.completed);
+        break;
+      case "Completed":
+        todoRemaining = allTodoList.filter((todo) => todo.completed);
+        break;
+      default:
+        //all
+        todoRemaining = allTodoList;
+        break;
+    }
+    // priority
+    if (priorities.length) {
+      todoRemaining = todoRemaining.filter((todo) => {
+        return priorities.includes(todo.priority);
+      });
+    }
+
+    return todoRemaining;
+  },
+);
